dfab2afb3442d1d995794d84b779d651dc6ae67c
SrikanthNabigari <srikanthnabigari108@outlook.com>
2016-10-14 23:10:02 +0530

    Changes made to add eroor methods

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
+// when google maps api failed to response
+var googleError = function(){
+    $('.api-error').show();
+};
+
 var map ;
 var infowindow ;
 initMap = function(){
-    
-
     var pyrmont = {lat: 17.3850, lng: 78.4867};
-
+    // displays the requested map content in map div 
     map = new google.maps.Map(document.getElementById('map'), {
         center: pyrmont,
         zoom: 12
     });
-
+    // displays location information in window when marker clicked
     infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
+    // gets the detailed place information by place_id stored in model
     for(var i = 0; i < places.length; i++){
             service.getDetails({
                 placeId:  places[i].place_id
@@ -21,7 +25,6 @@ initMap = function(){
                     map: map,
                     position: place.geometry.location
                 });
-                self.markersArray.push(marker);
                 google.maps.event.addListener(marker, 'click', function() {
                 infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
                                       place.formatted_address + '</div>');
@@ -32,42 +35,33 @@ initMap = function(){
         };       
 
 };
+
 var viewModel = function(){
     var self = this;
-
-    this.markersArray = ko.observableArray([]);
-    this.removeMarkers = function(){
-        for(var i=0; i<self.markersArray().length; i++ ){
-            self.markersArray()[i].setMap(null);
-        }
-    };
+    
     this.query = ko.observable('');
-
+    // filters the places array when searched in a query input
     this.searchResults = ko.computed(function() {
         q = self.query();
         return places.filter(function(i) {
             return i.name.toLowerCase().indexOf(q) >= 0;
         });
     });
-
+    // when name of the location clicked displays infowindow
     this.viewPlace = function(clickedName){
-        self.removeMarkers();
         var service = new google.maps.places.PlacesService(map);
         service.getDetails({
             placeId:  clickedName.place_id
         },function(place){
-            var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
+            self.marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                                  place.formatted_address + '</div>');
+            infowindow.open(map,self.marker);
         });
-        self.markersArray.push(marker);
-        console.log(self.markersArray());
-        google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-                                place.formatted_address + '</div>');
-        infowindow.open(map, this);
-        });
-        });
+        
     };
       
         
