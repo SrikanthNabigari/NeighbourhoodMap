667777578dff83c7c650532fad53f3925758083f
SrikanthNabigari <srikanthnabigari108@outlook.com>
2016-10-17 19:38:24 +0530

    Foursquare Api added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 // when google maps api gets failed
 var googleError = function(){
-    viewModel.self.apiError = true;
+    self.error_message('Failed to load GougleMaps Api');
+    self.apiError(true);
 };
-
+// when google maps api gets failed
+var FourSquareError = function(){
+    self.error_message('Failed to load Foursquare Api');
+    self.apiError(true);
+};
 
 var map ;
 var infowindow ;
-var marker;
+this.marker;
 initMap = function(){
     var pyrmont = {lat: 17.3850, lng: 78.4867};
     // displays the requested map content in map div 
@@ -24,7 +29,6 @@ initMap = function(){
 };
 // Adds the marker by getting place location details
 var AddMarker = function(place){
-    removeMarkers();
     var service = new google.maps.places.PlacesService(map);
         service.getDetails({
             placeId:  place.place_id
@@ -35,15 +39,16 @@ var AddMarker = function(place){
                 position: place.geometry.location
             });
             }
-        if(marker){
-            self.markersArray().push(marker);        
-            google.maps.event.addListener(marker, 'click', function() {
+        if(self.marker){
+            self.markersArray().push(self.marker);        
+            google.maps.event.addListener(self.marker, 'click', function() {
                 infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-                                       place.formatted_address + '</div>');
+                place.formatted_address + '</div>');
                 infowindow.open(map, this);
-            }); 
-        }   
+            });
+        } 
     });
+    
 };
 // removes all the markers
 var removeMarkers = function(){
@@ -56,12 +61,73 @@ var showMarkers = function(){
     for(var i=0; i<self.markersArray().length; i++ ){
         self.markersArray()[i].setMap(map);
     }
-};         
+}; 
+var open_infowindow = function(place,marker){
+    self.markersArray().push(marker); 
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                place.formatted_address + '</div>');
+                infowindow.open(map, marker);
+    google.maps.event.addListener(self.marker, 'click', function() {
+                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                place.formatted_address + '</div>');
+                infowindow.open(map, this);
+            });
+            
+};   
+
+// Gets the location data from Foursquare
+var locationData = function(place){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    
+    if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+    var today = ""+yyyy+mm+dd+"";
+
+    var venue_id = place.venue_id;
+    var client_id = "BWUSFIWLOPWY0F3RPSBEER3Y5AVNORL4SJ5UUH2LG2RLUSIH";
+    var client_secret = "0DFTYZIDWMTDRMGHMSXZ1RZL12IEMDBGWVSAA0XNJ0K1R3AV";
+    var FoursquareUrl = "https://api.foursquare.com/v2/venues/"+venue_id+"?client_id="+client_id+"&client_secret="+client_secret+"&v="+today+"" ;
+    
+    $.ajax({
+        url:FoursquareUrl,
+        dataType:"json",
+        async:true        
+    }).success(function(data){
+            self.des_name(data.response.venue.name);
+            self.rating(data.response.venue.rating);
+            var image_prefix = data.response.venue.bestPhoto.prefix;
+            var image_suffix = data.response.venue.bestPhoto.suffix;
+            self.location_image(image_prefix +"320x200"+ image_suffix);
+            if(data.response.venue.tips.groups[0].items[0].text){
+                self.review(data.response.venue.tips.groups[0].items[0].text);
+            }   
+    }).error(function(data){
+        FourSquareError();
+    })
+    
+};
+
+
+
+
 
 var viewModel = function(){
     var self = this;
     this.markersArray = ko.observableArray([]);    
     this.query = ko.observable('');
+    this.location_image = ko.observable();
+    this.des_name = ko.observable();
+    this.rating = ko.observable();
+    this.review = ko.observable();
+    this.apiError = ko.observable(false);
+    this.error_message = ko.observable();
     // filters the places array when searched in a query input
     this.searchResults = ko.computed(function() {
         q = self.query();
@@ -70,6 +136,7 @@ var viewModel = function(){
             return places;
         }
         else{
+            removeMarkers();
             return ko.utils.arrayFilter(places, function(place) {
                 if(place.name.toLowerCase().indexOf(q) >= 0) {
                     AddMarker(place);
@@ -82,24 +149,19 @@ var viewModel = function(){
     // when name of the location clicked displays infowindow
     this.viewPlace = function(clickedName){
         var service = new google.maps.places.PlacesService(map);
+        locationData(clickedName);
         service.getDetails({
             placeId:  clickedName.place_id
         },function(place){
-            marker = new google.maps.Marker({
+            self.marker = new google.maps.Marker({
                 map: map,
                 position: place.geometry.location
             });
-            if(marker){
-            self.markersArray().push(marker);        
-            }
-            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-                                  place.formatted_address + '</div>');
-            infowindow.open(map, marker);
+            open_infowindow(place,self.marker);
+            
         });
-        
     };
-      
-        
-};    
-
-ko.applyBindings(viewModel);
+};     
+    
+    
+ko.applyBindings(viewModel);  
--- js/model.js
+++ js/model.js
@@ -1,54 +1,62 @@
 var places = [
     {
         "name": "Golconda Fort",
-        "place_id": "ChIJ80ITX4yWyzsR34Tevb2pUtc"
+        "place_id": "ChIJ80ITX4yWyzsR34Tevb2pUtc",
+        "venue_id": "4c42eee8d7fad13ab4d009da"
     },
     {
         "name": "Char Minar",
-        "place_id": "ChIJm5MaboqXyzsR-xPxquRpWss"
+        "place_id": "ChIJm5MaboqXyzsR-xPxquRpWss",
+        "venue_id": "4e1d712415207c4cbec540bd"
     },
     {
         "name": "Qutub Shahi Tombs",
-        "place_id": "ChIJ-6Y1xJmWyzsR94DFFG3f2OI"
+        "place_id": "ChIJ-6Y1xJmWyzsR94DFFG3f2OI",
+        "venue_id": "4e227784fa761d671087a5ef"
     },
     {
         "name": "Nehru Zoological Park",
-        "place_id": "ChIJY8IDjM-XyzsRMjt5CbX0zT0"
+        "place_id": "ChIJY8IDjM-XyzsRMjt5CbX0zT0",
+        "venue_id": "4c4269da520fa593d043cbac"
     },
     {
         "name": "Salar Jung Museum",
-        "place_id": "ChIJu9dj4oeXyzsRQaBpm0uTOzU"
+        "place_id": "ChIJu9dj4oeXyzsRQaBpm0uTOzU",
+        "venue_id": "4bc95d44fb84c9b6d5101b3e"
     },
     {
         "name": "NTR Gardens",
-        "place_id": "ChIJgQjtSlqXyzsRdTamKL7_edo"
+        "place_id": "ChIJgQjtSlqXyzsRdTamKL7_edo",
+        "venue_id":"4d8f365f788c54817cf549fd"
     },
     {
         "name": "Falaknuma Palace",
-        "place_id": "ChIJs41EWFa9yzsRu3DBVQtXsKQ"
+        "place_id": "ChIJs41EWFa9yzsRu3DBVQtXsKQ",
+        "venue_id": "4d0103874f56b60cf6f6a437"
     },
     {
         "name": "Lumbini Park",
-        "place_id": "ChIJpeyxLF-XyzsRXBhk5vcfEmU"
-    },
-    {
-        "name": "Hussain Sagar",
-        "place_id": "ChIJa5sujFWXyzsRnU6EzGNTcCU"
+        "place_id": "ChIJpeyxLF-XyzsRXBhk5vcfEmU",
+        "venue_id": "4c62b4daeb82d13a98f405d6"
     },
     {
         "name": "Prasads IMAX",
-        "place_id": "ChIJ3fpK-1qXyzsRhGzfB4TqiQ8"
+        "place_id": "ChIJ3fpK-1qXyzsRhGzfB4TqiQ8",
+        "venue_id": "4b72b33bf964a52054842de3"
     },
     {
         "name": "Mecca Masjid",
-        "place_id": "ChIJMTUJX4qXyzsR4u4HAHn34Hg"
+        "place_id": "ChIJMTUJX4qXyzsR4u4HAHn34Hg",
+        "venue_id": "4c727c129e706dcbf8dd4a62"
     },
     {
         "name": "KBR National Park",
-        "place_id": "ChIJZe78QtKWyzsRA7_hetcQNG4"
+        "place_id": "ChIJZe78QtKWyzsRA7_hetcQNG4",
+        "venue_id": "4ce914cbf3bda14382d2bde4"
     },
     {
         "name": "Birla Mandir",
-        "place_id": "ChIJBcAk0V2XyzsRjing1vqfQ1k"
+        "place_id": "ChIJBcAk0V2XyzsRjing1vqfQ1k",
+        "venue_id": "4be4c4478a8cb7139351c4a0"
     }
 ]
